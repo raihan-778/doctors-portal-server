a836e3a9834de96c09743e16dfd64a662dd91dbf
unknown <raihanarif778@gmail.com>
2022-11-20 22:56:22 +0600

    api for deshboard functionality added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { application } = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,7 +16,7 @@ app.use(express.json());
 auth info
 
 "doctorsPortal"
-"KsdFHtqDql5WHPKR"
+"cIkloK1kq6AInKCs"
  */
 
 //mongodb connection
@@ -26,6 +27,21 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorised access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbiden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 async function run() {
   try {
@@ -68,11 +84,16 @@ async function run() {
     }); //use this api or below v2 api. both are smae
     //get api for bookings for single person
 
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", verifyJWT, async (req, res) => {
       const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        res.status(403).send({ message: "forbidden access" });
+      }
       const query = {
         email: email,
       };
+
       const bookings = await bookingCollection.find(query).toArray();
       res.send(bookings);
     });
@@ -128,7 +149,6 @@ async function run() {
 
     app.post("/booking", async (req, res) => {
       const booking = req.body;
-      console.log(booking);
       const query = {
         appointmentDate: booking.appointmentDate,
         treatment: booking.treatment,
@@ -140,6 +160,48 @@ async function run() {
         return res.send({ acknowledged: false, message });
       }
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    //get api for geting user info
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
+    //get api to check if the user is admin or not
+
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    //api for admin role to update user info
+
+    app.put("/users/admin/:id", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send("forbidden Access");
+      }
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
       res.send(result);
     });
 
@@ -156,8 +218,14 @@ async function run() {
       const email = req.query.email;
       const query = { email: email };
       const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
       console.log(user);
-      res.send({ token: "acces token" });
+      res.status(403).send({ accessToken: "unauthorised User" });
     });
   } finally {
   }
